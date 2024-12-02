8e8cee673d0670b9bad18dd0fecf09a88ea54abc
5sj20131 <20131@hakodate.kosen-ac.jp>
2024-12-02 13:48:25 +0900

    スタート画面内のプログラム整理

--- game10/MyWorld.java
+++ game10/MyWorld.java
@@ -62,20 +62,15 @@ public class MyWorld extends World
      * 
      */
 
-<<<<<<< HEAD
-=======
+
     
 
->>>>>>> 10ed2b8d8fbacf7e7f69e75272506d76dc210456
+
       
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         
 
-<<<<<<< HEAD
-    
-=======
-       
->>>>>>> 10ed2b8d8fbacf7e7f69e75272506d76dc210456
+
 
         @Override
         public void draw(Graphics2D g, int cellsize)
@@ -117,24 +112,21 @@ public class MyWorld extends World
         super(1200, 676, 1);
         getBackground().scale( 1200, 676 );
         showTextEx("路上格闘", 600, 226, 200, true, Color.RED );
-<<<<<<< HEAD
 
-        addObject( new Life(), 1000 , 50 );
 
-        addObject( new Start(), 600, 476 );
 
-=======
-<<<<<<< HEAD
+       
+
+
 
         
 
+       
+
+
+        
         addObject( new Start(), 600, 476 );
 
-=======
-        addObject( new Life(), 1000 , 50 );
-        addObject( new Start(), 600, 476 );
->>>>>>> 4d5daa317c4c72e22fa7b4a30196cc5677c6d1fb
->>>>>>> 10ed2b8d8fbacf7e7f69e75272506d76dc210456
     }
     
 }
