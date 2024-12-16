316885b4516311a0e2240a24806793ff7a4e1913
5sj20131 <20131@hakodate.kosen-ac.jp>
2024-12-16 14:11:07 +0900

    波動をランダムで固定された行に放つ

--- game10/Button_Result.java
+++ game10/Button_Result.java
@@ -6,9 +6,35 @@ public class Button_Result extends Actor
    
     public void act() 
     {
-        int x0 = getX();
-         int y0 = getY();
+        MouseInfo minfo = Greenfoot.getMouseInfo();
          
+          int x0 = getX();
+         int y0 = getY();
+
+         int w = getImage().getWidth();
+         int h = getImage().getHeight();
+         
+         
+        ((Gameover)getWorld()).showTextEx("スコアを表示", x0-13, y0, 56, false, Color.CYAN );
         
+         if( minfo != null ){
+
+            // マウス座標取得
+            int x = minfo.getX();
+            int y = minfo.getY();
+            
+            
+            // マウスボタンのクリック時に押したボタン番号を取得
+            // クリックした時だけ数値が入る。押しっぱなしの時やボタンを離した時は 0 になるので注意
+            int button = minfo.getButton();
+            if( button != 0 ){
+                if((x>=x0-w/2&&x<=x0+w/2)&&(y>=y0-h/2&&y<=y0+h/2))
+                {
+                    
+                    
+                }
+                
+            }
+        }
     }    
 }
