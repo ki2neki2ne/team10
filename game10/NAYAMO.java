afe8dc7828792bec18784683b7ee07376e7de2a1
ki2neki2ne <huwa2ki@gmail.com>
2025-01-31 10:57:56 +0900

    NAYAMO透過差し替え

--- game10/NAYAMO.java
+++ game10/NAYAMO.java
@@ -8,6 +8,14 @@
      */
     public class NAYAMO extends Actor
     {
+        private int gravity;
+        private void checkForJump()
+{
+    Actor a = getOneIntersectingObject(senntou.class);
+    if (a != null) {
+         gravity = 20; // this will make the character jump
+    }
+}      
         /**
          * Act - do whatever the yamano wants to do. This method is called whenever
          * the 'Act' or 'Run' button gets pressed in the environment.
@@ -21,50 +29,13 @@
     int l = 0;
     int r = 0;
     int gado = 0;
+    int jump = 0;
+    int tcnt = 0;
+    
+   
     if( Greenfoot.isKeyDown( "w" ) ){
-        if(Greenfoot.isKeyDown( "w" )&&Greenfoot.isKeyDown( "a" ))
-        {
-            for(int i = 0; i<=30; i++){
-              setLocation( x-l,y-i*6);
-              Greenfoot.delay(1);
-              n++;
-              l+=2;
-            }
-            for(int m = 0; m<=10; m++){
-                setLocation( x-m,y);
-            }
-            for(int b = n; n>=0; n--){
-              setLocation( x-l,y-n*6);
-              Greenfoot.delay(1);
-              l+=2;
-            }
-        }else if(Greenfoot.isKeyDown( "w" )&&Greenfoot.isKeyDown( "d" ))
-        {
-            for(int i = 0; i<=30; i++){
-              setLocation( x+r,y-i*6);
-              Greenfoot.delay(1);
-              n++;
-              r+=2;
-            }
-            for(int m = 0; m<=10; m++){
-                setLocation( x+m,y);
-            }
-            for(int b = n; n>=0; n--){
-              setLocation( x+r,y-n*6);
-              Greenfoot.delay(1);
-              r+=2;
-            }
-        }else if( Greenfoot.isKeyDown( "w" ) ){
-            for(int i = 0; i<=30; i++){
-              setLocation( x,y-i*6);
-              Greenfoot.delay(1);
-              n++;
-            }
-            for(int b = n; n>=0; n--){
-              setLocation( x,y-n*6);
-              Greenfoot.delay(1);
-            }
-        }
+        
+            
     }
     if( Greenfoot.isKeyDown( "a" ) ){
             if(Greenfoot.isKeyDown("a")&&Greenfoot.isKeyDown("s"))
